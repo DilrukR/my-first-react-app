325711615dceb4b193f038b1994acf070ce6257d
DilrukR <68820399+DilrukR@users.noreply.github.com>
2022-04-14 01:01:24 +0530

    second

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Counter extends React.Component {
+    render() { 
+        return <div >
+            <h1>Counter</h1>
+            <button>Rishan</button>
+        </div>;
+    }
+}
+ 
+export default Counter;
+
